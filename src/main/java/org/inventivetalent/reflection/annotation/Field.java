9972895a7f857ee128769d78b2cd0af4e089738f
inventivetalent <inventivetalent@inventivegames.de>
2016-06-19 16:15:26 +0200

    Add annotations to load @Class, @Field and @Method

---
+++ src/main/java/org/inventivetalent/reflection/annotation/Field.java
@@ -0,0 +1,21 @@
+package org.inventivetalent.reflection.annotation;
+
+import org.inventivetalent.reflection.minecraft.Minecraft;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Field {
+
+	String className();
+
+	String[] value();
+
+	Minecraft.Version[] versions() default {};
+
+	boolean ignoreExceptions() default true;
+}
