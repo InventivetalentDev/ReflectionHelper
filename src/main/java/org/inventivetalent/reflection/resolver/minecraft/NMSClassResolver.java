21f090f33f339a65eb100466cf204f3023858e92
inventivetalent <mail@inventivetalent.org>
2021-06-15 20:43:14 +0200

    .

--- src/main/java/org/inventivetalent/reflection/resolver/minecraft/NMSClassResolver.java
+++ src/main/java/org/inventivetalent/reflection/resolver/minecraft/NMSClassResolver.java
@@ -11,8 +11,8 @@ public class NMSClassResolver extends ClassResolver {
 	@Override
 	public Class resolve(String... names) throws ClassNotFoundException {
 		for (int i = 0; i < names.length; i++) {
-			if (!names[i].startsWith("net.minecraft.server")) {
-				names[i] = "net.minecraft.server." + Minecraft.getNMSVersion() + names[i];
+			if (!names[i].startsWith("net.minecraft")) {
+				names[i] = Minecraft.getNMSPackage() + "." + names[i];
 			}
 		}
 		return super.resolve(names);
--- src/main/java/org/inventivetalent/reflection/resolver/minecraft/OBCClassResolver.java
+++ src/main/java/org/inventivetalent/reflection/resolver/minecraft/OBCClassResolver.java
@@ -8,13 +8,13 @@ import org.inventivetalent.reflection.resolver.ClassResolver;
  */
 public class OBCClassResolver extends ClassResolver {
 
-	@Override
-	public Class resolve(String... names) throws ClassNotFoundException {
-		for (int i = 0; i < names.length; i++) {
-			if (!names[i].startsWith("org.bukkit.craftbukkit")) {
-				names[i] = "org.bukkit.craftbukkit." + Minecraft.getNMSVersion() + names[i];
-			}
-		}
-		return super.resolve(names);
-	}
+    @Override
+    public Class resolve(String... names) throws ClassNotFoundException {
+        for (int i = 0; i < names.length; i++) {
+            if (!names[i].startsWith("org.bukkit")) {
+                names[i] = Minecraft.getOBCPackage() + "." + names[i];
+            }
+        }
+        return super.resolve(names);
+    }
 }
