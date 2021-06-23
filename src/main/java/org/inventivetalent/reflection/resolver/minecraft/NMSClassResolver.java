a75f5be538dcd339da97439158c420fd520dcfc8
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2021-06-23 12:03:29 +0300

    Enhance NMS Class Resolver (#48)

--- src/main/java/org/inventivetalent/reflection/resolver/minecraft/NMSClassResolver.java
+++ src/main/java/org/inventivetalent/reflection/resolver/minecraft/NMSClassResolver.java
@@ -1,6 +1,7 @@
 package org.inventivetalent.reflection.resolver.minecraft;
 
 import org.inventivetalent.reflection.minecraft.Minecraft;
+import org.inventivetalent.reflection.minecraft.MinecraftVersion;
 import org.inventivetalent.reflection.resolver.ClassResolver;
 
 /**
@@ -13,6 +14,16 @@ public class NMSClassResolver extends ClassResolver {
 		for (int i = 0; i < names.length; i++) {
 			if (!names[i].startsWith("net.minecraft")) {
 				names[i] = Minecraft.getNMSPackage() + "." + names[i];
+			} else if (names[i].contains(".")) {
+				/* name contains dot but don't start with NMS (ex: world.entity.Entity) */
+				if (MinecraftVersion.VERSION.hasNMSVersionPrefix()) {
+					/* use class name only */
+					String[] path = names[i].split("\\.");
+					names[i] = Minecraft.getNMSPackage() + "." + path[path.length - 1];
+				} else {
+					/* use the whole name */
+					names[i] = Minecraft.getNMSPackage() + "." + names[i];
+				}
 			}
 		}
 		return super.resolve(names);
