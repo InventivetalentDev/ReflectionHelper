e56e478e9ba1948486a9b248b13428a3498e0ab8
JerryLin <41278925+lulu2002@users.noreply.github.com>
2024-05-04 03:44:32 +0800

    feat: add finding for super class in MethodResolver and FieldResolver (#62)
    * feat: add finding for super class in MethodResolver and FieldResolver, add gitignore
    
    * chore: wrap print stack trace in resolver abstract with VERBOSE check
    
    * chore: remove debug message
    
    * feat: add tests to making sure resolver will firstly look for current class instead of super one

--- src/main/java/org/inventivetalent/reflection/resolver/ResolverAbstract.java
+++ src/main/java/org/inventivetalent/reflection/resolver/ResolverAbstract.java
@@ -56,6 +56,8 @@ public abstract class ResolverAbstract<T> {
 				return resolved;
 			} catch (ReflectiveOperationException e) {
 				//Not found, ignore the exception
+				if (AccessUtil.VERBOSE)
+					e.printStackTrace();
 			}
 		}
 
--- src/test/java/org/inventivetalent/reflectionhelper/test/ResolverTest.java
+++ src/test/java/org/inventivetalent/reflectionhelper/test/ResolverTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import static org.junit.Assert.assertEquals;
@@ -16,24 +17,67 @@ import static org.junit.Assert.assertNotNull;
 
 public class ResolverTest {
 
-	@Test
-	public void basic() throws ReflectiveOperationException{
-		ClassResolver classResolver = new ClassResolver();
-		Class clazz = classResolver.resolve("org.inventivetalent.reflection.resolver.wrapper.ClassWrapper");
-		assertNotNull(clazz);
-		assertEquals(ClassWrapper.class, clazz);
+    @Test
+    public void basic() throws ReflectiveOperationException {
+        ClassResolver classResolver = new ClassResolver();
+        Class clazz = classResolver.resolve("org.inventivetalent.reflection.resolver.wrapper.ClassWrapper");
+        assertNotNull(clazz);
+        assertEquals(ClassWrapper.class, clazz);
 
-		FieldResolver fieldResolver = new FieldResolver(clazz);
-		Field field = fieldResolver.resolve("clazz");
-		assertNotNull(field);
+        FieldResolver fieldResolver = new FieldResolver(clazz);
+        Field field = fieldResolver.resolve("clazz");
+        assertNotNull(field);
 
-		ConstructorResolver constructorResolver = new ConstructorResolver(clazz);
-		Constructor constructor = constructorResolver.resolveFirstConstructor();
-		assertNotNull(constructor);
+        ConstructorResolver constructorResolver = new ConstructorResolver(clazz);
+        Constructor constructor = constructorResolver.resolveFirstConstructor();
+        assertNotNull(constructor);
 
-		MethodResolver methodResolver = new MethodResolver(clazz);
-		Method method = methodResolver.resolve("newInstance");
-		assertNotNull(method);
-	}
+        MethodResolver methodResolver = new MethodResolver(clazz);
+        Method method = methodResolver.resolve("newInstance");
+        assertNotNull(method);
+    }
+
+    @Test
+    public void shouldResolveSuperField() throws NoSuchFieldException, NoSuchMethodException {
+        FieldResolver fieldResolver = new FieldResolver(SubClass.class);
+        Field field = fieldResolver.resolve("a");
+        assertNotNull(field);
+
+        MethodResolver methodResolver = new MethodResolver(SubClass.class);
+        Method method = methodResolver.resolve("a");
+        assertNotNull(method);
+    }
+
+    @Test
+    public void shouldIgnoreSuperclassIfFoundInSubclass() throws NoSuchFieldException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        BaseClass baseClass = new BaseClass();
+        SubClass subClass = new SubClass();
+
+        FieldResolver fieldResolver = new FieldResolver(SubClass.class);
+        Field field = fieldResolver.resolve("b");
+
+        MethodResolver methodResolver = new MethodResolver(SubClass.class);
+        Method method = methodResolver.resolve("a");
+
+        assertEquals("sub-b", field.get(subClass));
+        assertEquals(2, method.invoke(subClass));
+    }
+
+    class BaseClass {
+        private String a = "base";
+        private String b = "base-b";
+
+        private int a() {
+            return 1;
+        }
+    }
+
+    class SubClass extends BaseClass {
+        private String b = "sub-b";
+
+        private int a() {
+            return 2;
+        }
+    }
 
 }
